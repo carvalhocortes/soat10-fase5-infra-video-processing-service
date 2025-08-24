81d3c3a5aa1c009c9e9d733d6d0284f579b18cc1
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-23 21:42:35 -0300

    Add outputs for S3 bucket name and ARN in Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,17 @@ output "SQS_VIDEO_FILES_QUEUE_URL" {
   value       = aws_sqs_queue.sqs-video-files.id
 }
 
-
 output "SNS_VIDEO_FILES_TOPIC_ARN" {
   description = "ARN do tópico SNS video-files"
   value       = aws_sns_topic.video-files.arn
+}
+
+output "S3_VIDEO_FILES_BUCKET_NAME" {
+  description = "Nome do bucket S3 de arquivos de vídeo"
+  value       = aws_s3_bucket.fiap_video_files.bucket
+}
+
+output "S3_VIDEO_FILES_BUCKET_ARN" {
+  description = "ARN do bucket S3 de arquivos de vídeo"
+  value       = aws_s3_bucket.fiap_video_files.arn
 }
