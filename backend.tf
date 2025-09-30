105fbb1286e6d179ab604674a62de4b9d1eef106
CarvalhoCortes <carvalhocortes@gmail.com>
2025-09-29 22:03:23 -0300

    Refactor GitHub Actions workflows and update backend configuration for Terraform cleanup

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-soat10-bucket-fase5"
+    bucket = "fiap-terraform-backend"
     key    = "github-actions-fiap/infra-video-manager/terraform.tfstate"
 
     region  = "us-west-2"
