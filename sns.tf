221aacbdc660f7dbbdcbb0977c74bd5bc2c245d8
Samuel Victor <samuel@samuelvictor.dev>
2025-09-30 22:10:38 -0300

    add sns sqs

--- sns.tf
+++ sns.tf
@@ -1,9 +1,19 @@
-resource "aws_sns_topic" "video-files" {
-  name = "sns-video-files"
+resource "aws_sns_topic" "sns-video-processing" {
+  name = "sns-video-processing"
 }
 
-resource "aws_sns_topic_subscription" "video-files" {
-  topic_arn = aws_sns_topic.video-files.arn
+resource "aws_sns_topic" "sns-video-manager-api" {
+  name = "sns-video-manager-api"
+}
+
+resource "aws_sns_topic_subscription" "video-processing-to-manager-api" {
+  topic_arn = aws_sns_topic.sns-video-processing.arn
   protocol  = "sqs"
-  endpoint  = aws_sqs_queue.sqs-video-files.arn
+  endpoint  = aws_sqs_queue.sqs-video-manager-api.arn
+}
+
+resource "aws_sns_topic_subscription" "video-manager-api-to-processing" {
+  topic_arn = aws_sns_topic.sns-video-manager-api.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.sqs-video-processing.arn
 }
--- sqs.tf
+++ sqs.tf
@@ -1,9 +1,13 @@
-resource "aws_sqs_queue" "sqs-video-files" {
-  name = "sqs-video-files"
+resource "aws_sqs_queue" "sqs-video-processing" {
+  name = "sqs-video-processing"
 }
 
-resource "aws_sqs_queue_policy" "sqs-video-files-policy" {
-  queue_url = aws_sqs_queue.sqs-video-files.id
+resource "aws_sqs_queue" "sqs-video-manager-api" {
+  name = "sqs-video-manager-api"
+}
+
+resource "aws_sqs_queue_policy" "sqs-video-processing-policy" {
+  queue_url = aws_sqs_queue.sqs-video-processing.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -14,10 +18,33 @@ resource "aws_sqs_queue_policy" "sqs-video-files-policy" {
           Service = "sns.amazonaws.com"
         }
         Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.sqs-video-files.arn
+        Resource = aws_sqs_queue.sqs-video-processing.arn
         Condition = {
           ArnEquals = {
-            "aws:SourceArn" = aws_sns_topic.video-files.arn
+            "aws:SourceArn" = aws_sns_topic.sns-video-manager-api.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "sqs-video-manager-api-policy" {
+  queue_url = aws_sqs_queue.sqs-video-manager-api.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.sqs-video-manager-api.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.sns-video-processing.arn
           }
         }
       }
